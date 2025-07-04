b3504f32d06514dbcd2eb373a9d18b3e143e9a8d
Jeevan-coder-AI <jeevansakthi2020@gmail.com>
2025-07-04 13:37:55 +0530

    gt

---
+++ test.py
@@ -0,0 +1,8 @@
+from gpiozero import DigitalInputDevice,DigitalOutputDevice
+from time import sleep
+buzz=DigitalOutputDevice(14)
+while True:
+    buzz.on()
+    sleep(0.1)
+    buzz.off()               
+    sleep(0.1)
